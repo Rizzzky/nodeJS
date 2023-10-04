fef33f1e181abe8135817a2790eec79fe860fbe2
Rizzzky <114666981+Rizzzky@users.noreply.github.com>
2023-10-04 17:57:20 +0700

    Tugas Sesi 2 Pemrograman Berbasis Platform

---
+++ Tugas2.js
@@ -0,0 +1,6 @@
+var firstName = "Mochamad Rizky" 
+var lastName = "Fauzi"   
+var age = 30           
+var isMarried = false
+
+console.log(`Nama: ${firstName} ${lastName}\nUmur: ${age}\nStatus Pernikahan: ${isMarried ? 'Sudah Menikah' : 'Belum Menikah'}`);
