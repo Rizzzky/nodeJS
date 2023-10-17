74f4cc5601f7359b89acdd26f6da8a29a7d66808
Rizzzky <114666981+Rizzzky@users.noreply.github.com>
2023-10-17 22:21:57 +0700

    Add files via upload

---
+++ restaurant.js
@@ -0,0 +1,9 @@
+const restaurant = {
+    name: "Nama Restoran",
+    city: "Nama Kota",
+    favoriteDrink: "Minuman Favorit",
+    favoriteFood: "Makanan Favorit",
+    isVegan: true 
+
+  };
+console.log(restaurant);
---
+++ restaurants.js
@@ -0,0 +1,24 @@
+let restaurants = [
+    {
+      name: "Restoran1",
+      city: "Kota1",
+      favoriteDrink: "MinumanFavorit1",
+      favoriteFood: "MakananFavorit1",
+      isVegan: true
+    },
+    {
+      name: "Restoran2",
+      city: "Kota2",
+      favoriteDrink: "MinumanFavorit2",
+      favoriteFood: "MakananFavorit2",
+      isVegan: false
+    },
+    {
+      name: "Restoran3",
+      city: "Kota3",
+      favoriteDrink: "MinumanFavorit3",
+      favoriteFood: "MakananFavorit3",
+      isVegan: true
+    }
+  ];
+console.log(restaurants)
